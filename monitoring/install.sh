050bfb00fdef00225b6bb60f234dac4a770bf874
tarun teja <tarunteja@taruns-MacBook-Pro.local>
2026-07-15 05:46:01 +0530

    Chore: Add monitoring

---
+++ monitoring/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+
+helm repo update
+
+helm install monitoring prometheus-community/kube-prometheus-stack \
+  -f prometheus-values.yaml \
+  -n monitoring \
+  --create-namespace
